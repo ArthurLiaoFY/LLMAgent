56642e871bfae8c0d2adfa4c29dbc31d5825a7ff
arthur <wr80340@smail.nchu.edu.tw>
2025-04-08 16:16:07 +0800

    #no bug

--- add_multiply_agents.py
+++ add_multiply_agents.py
@@ -1,72 +1,40 @@
 # %%
 
-from typing import Annotated
+from typing import Annotated, Union
 
 import numpy as np
 from IPython.display import Image, display
+from langchain.cache import InMemoryCache
+from langchain.globals import set_debug, set_llm_cache
 from langchain_core.messages import ToolMessage, convert_to_messages
 from langchain_core.tools import tool
 from langchain_core.tools.base import InjectedToolCallId
 from langchain_ollama import ChatOllama
 from langgraph.graph import START, MessagesState, StateGraph
 from langgraph.prebuilt import InjectedState, create_react_agent
+from langgraph.prebuilt.chat_agent_executor import AgentState
 from langgraph.types import Command
-from langgraph_supervisor import create_supervisor
+from langgraph_supervisor import create_handoff_tool, create_supervisor
+from pydantic import BaseModel, Field
 from typing_extensions import Literal
 
+set_llm_cache(InMemoryCache())
+set_debug(False)
+# %%
 model = ChatOllama(
     model="qwen2.5:14b",
     temperature=0,
 )
 
 
-def pretty_print_messages(stream_chunk):
-    if isinstance(stream_chunk, tuple):
-        ns, stream_chunk = stream_chunk
-        # skip parent graph updates in the printouts
-        if len(ns) == 0:
-            return
-
-        graph_id = ns[-1].split(":")[0]
-        print(f"Update from subgraph {graph_id}:")
-        print("\n")
-
-    for node_name, node_update in stream_chunk.items():
-        print(f"Update from node {node_name}:")
-        print("\n")
-
-        for m in convert_to_messages(node_update["messages"]):
-            m.pretty_print()
-        print("\n")
+class Response(BaseModel):
+    mathematic_answer: Union[float, int] = Field(
+        description="the answer of the math equation."
+    )
 
 
-def make_handoff_tool(*, agent_name: str):
-    """Create a tool that can return handoff via a Command"""
-    tool_name = f"transfer_to_{agent_name}"
-
-    @tool(tool_name)
-    def handoff_to_agent(
-        state: Annotated[dict, InjectedState],
-        tool_call_id: Annotated[str, InjectedToolCallId],
-    ):
-        """Ask another agent for help."""
-        tool_message = {
-            "role": "tool",
-            "content": f"Successfully transferred to {agent_name}",
-            "name": tool_name,
-            "tool_call_id": tool_call_id,
-        }
-        return Command(
-            # navigate to another agent node in the PARENT graph
-            goto=agent_name,
-            graph=Command.PARENT,
-            # This is the state update that the agent `agent_name` will see when it is invoked.
-            # We're passing agent's FULL internal message history AND adding a tool message to make sure
-            # the resulting chat history is valid.
-            update={"messages": state["messages"] + [tool_message]},
-        )
-
-    return handoff_to_agent
+class GraphState(AgentState):
+    structured_response: Response
 
 
 # %%
@@ -92,18 +60,32 @@ def mean(a: list) -> float:
 
 addition_agent = create_react_agent(
     model=model,
-    tools=[add, make_handoff_tool(agent_name="multiplication_agent")],
-    name="addition_expert",
+    tools=[
+        add,
+        create_handoff_tool(
+            agent_name="multiplication_agent",
+            name="assign to multiplication expert",
+            description="Assign task to multiplication expert",
+        ),
+    ],
+    name="addition_agent",
     prompt=(
         "You are an addition expert, you can ask the multiplication expert for help with multiplication. "
         "Always do your portion of calculation before the handoff."
     ),
+    state_schema=GraphState,
 )
-
 multiplication_agent = create_react_agent(
     model=model,
-    tools=[multiply, make_handoff_tool(agent_name="addition_agent")],
-    name="multiplication_expert",
+    tools=[
+        multiply,
+        create_handoff_tool(
+            agent_name="addition_agent",
+            name="assign to addition expert",
+            description="Assign task to addition expert",
+        ),
+    ],
+    name="multiplication_agent",
     prompt=(
         "You are an multiplication expert, you can ask the addition expert for help with addition. "
         "Always do your portion of calculation before the handoff."
@@ -113,8 +95,16 @@ supervisor = create_supervisor(
     agents=[addition_agent, multiplication_agent],
     model=model,
     tools=[
-        make_handoff_tool(agent_name="addition_agent"),
-        make_handoff_tool(agent_name="multiplication_agent"),
+        create_handoff_tool(
+            agent_name="addition_agent",
+            name="assign to addition expert",
+            description="Assign task to addition expert",
+        ),
+        create_handoff_tool(
+            agent_name="multiplication_agent",
+            name="assign to multiplication expert",
+            description="Assign task to multiplication expert",
+        ),
     ],
     prompt=(
         "You are a math team supervisor managing a addition expert and a multiplication expert. "
@@ -123,6 +113,8 @@ supervisor = create_supervisor(
         "For multiplication problems, call multiplication_agent for help. "
         "Do not solve the problem by yourself."
     ),
+    state_schema=GraphState,
+    response_format=Response,
 )
 
 
@@ -131,9 +123,10 @@ supervisor = create_supervisor(
 graph = supervisor.compile()
 
 # %%
-addition_agent.invoke(input={"messages": [("human", "1+5")]})
+display(Image(graph.get_graph(xray=True).draw_mermaid_png()))
+
 # %%
-for chunk in graph.stream(
+result = graph.invoke(
     {
         "messages": [
             {
@@ -142,10 +135,9 @@ for chunk in graph.stream(
             }
         ]
     }
-):
-    pretty_print_messages(chunk)
+)
 # %%
-for chunk in graph.stream(
+result = graph.invoke(
     {
         "messages": [
             {
@@ -154,6 +146,5 @@ for chunk in graph.stream(
             }
         ]
     }
-):
-    pretty_print_messages(chunk)
+)
 # %%
