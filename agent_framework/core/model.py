17afad1d41635d72b8da4b4285da680def6f7feb
arthur <wr80340@smail.nchu.edu.tw>
2025-04-10 10:57:56 +0800

    #add embd size and embd models

--- agent_framework/core/model.py
+++ agent_framework/core/model.py
@@ -4,11 +4,16 @@ from langchain_ollama import ChatOllama, OllamaEmbeddings
 
 with open("config.json") as f:
     config = json.loads(f.read())
-llm_vector_size = config.get("llm_model", {}).get("vector_size", "5120")
 llm_model = ChatOllama(
-    model=config.get("llm_model", {}).get("model_name", "deepseek-r1:14b"),
+    model=config.get("llm_model", {}).get("model_name"),
     temperature=0,
 )
-llm_embd = OllamaEmbeddings(
-    model=config.get("llm_model", {}).get("model_name", "deepseek-r1:14b"),
+llm_coder = ChatOllama(
+    model=config.get("llm_coder", {}).get("model_name"),
+    temperature=0,
 )
+embd_model = OllamaEmbeddings(
+    model=config.get("embd_model", {}).get("model_name"),
+)
+
+embd_size = config.get("embd_model", {}).get("embd_size")
