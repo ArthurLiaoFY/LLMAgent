b9a96e25c807239e36fb2bfdd33deef9e17b40ec
arthur <wr80340@smail.nchu.edu.tw>
2025-04-13 15:48:38 +0800

    #done

--- langgraph_tutorials/rewoo.py
+++ langgraph_tutorials/rewoo.py
@@ -5,9 +5,16 @@ import re
 from typing import Dict, List
 
 from IPython.display import Image, display
+from langchain.cache import InMemoryCache
+from langchain.globals import set_debug, set_llm_cache
 from langchain.tools import tool
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+)
 from langchain_core.runnables.base import RunnableSequence
 from langchain_core.tools.structured import StructuredTool
 from langchain_ollama import ChatOllama
@@ -15,19 +22,21 @@ from langgraph.graph import END, START, StateGraph
 from pydantic import BaseModel, Field
 from typing_extensions import TypedDict
 
+set_llm_cache(InMemoryCache())
+set_debug(True)
+
 with open("../secrets.json") as f:
     secrets = json.loads(f.read())
 
 with open("../config.json") as f:
     config = json.loads(f.read())
 
-
 os.environ["LANGSMITH_API_KEY"] = secrets.get("langsmith").get("api_key")
 os.environ["TAVILY_API_KEY"] = secrets.get("tavily").get("api_key")
 
 
 model = ChatOllama(model=config.get("llm_model").get("model_name"), temperature=0)
-travily_search = TavilySearchResults(max_results=3)
+tavily_search = TavilySearchResults(max_results=3)
 
 
 @tool
@@ -36,53 +45,78 @@ def chat_bot(query: str):
     You are a smart and friendly AI assistant. You handle general conversations, tasks, and instructions normally and directly.
     However, when a user asks a factual or knowledge-based question — especially about current events, news, specific information, or anything that may require up-to-date facts — you must NOT answer it yourself.
     """
+    return model.invoke({"query": query})
 
 
 @tool
-def travily_result_summarize(query: str):
+def redefine_tool_input(query: str, history_results: List[str]):
+    """This tool rewrites a user's follow-up query into a self-contained, context-independent question by referencing the provided historical information."""
+    redefine_prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessagePromptTemplate.from_template(
+                template="""
+                    You are an intelligent assistant that rewrites user questions based on the context provided.
+                    Given a passage and a follow-up query that refers to the passage, rewrite the query into a self-contained question, replacing all references with specific information from the passage.
+                """
+            ),
+            HumanMessagePromptTemplate.from_template(
+                template="""
+                    Passage:
+                    {history}
+
+                    Original Query:
+                    {query}
+
+                    Rewritten Query:
+                """
+            ),
+        ]
+    )
+    chain = redefine_prompt | model
+    return chain.invoke({"query": query, "history": ", ".join(history_results)})
+
+
+@tool
+def summarized_tavily_result(query: str):
     """
     A search engine optimized for comprehensive, accurate, and trusted results.
     Useful for when you need to answer questions about current events.
     Input should be a search query.
     And will summarizes and answers base on user questions.
     """
-    travily_search = TavilySearchResults(max_results=5)
+    tavily_search = TavilySearchResults(max_results=3)
+    search_results = tavily_search.invoke({"query": query})
     search_result_prompt = ChatPromptTemplate.from_messages(
         [
-            {
-                "role": "human",
-                "content": """
+            SystemMessagePromptTemplate.from_template(
+                template="""
                     You are a smart assistant that answers questions based on search engine results.
-                    Given a user question and a list of search results retrieved from Travily, 
+                    Given a user question and a list of search results retrieved from Tavily, 
                     extract the most relevant information to directly answer the question.
                     Keep your answer concise and factual. Include important numbers or descriptions if available. 
                     Do not add extra explanations or assumptions.
-                    """,
-            },
-            {
-                "role": "human",
-                "content": """
+                    """
+            ),
+            HumanMessagePromptTemplate.from_template(
+                template="""
                     User Question:
                     {query}
 
-                    Travily Search Results (sorted by relevance):
+                    Tavily search results:
                     {search_results}
-
-                    Based on the above information, provide a direct answer to the question. 
-                    If no relevant answer can be found, reply with: "The answer is not available based on the current search results."
-                """,
-            },
+                    """
+            ),
         ]
     )
     chain = search_result_prompt | model
 
-    return chain.invoke(
-        {"query": query, "search_results": travily_search.invoke({"query": query})}
-    )
+    return chain.invoke({"query": query, "search_results": search_results})
 
 
 # %%
-tools = [chat_bot, travily_result_summarize]
+
+# %%
+tools = [chat_bot, summarized_tavily_result]
 
 # %%
 
@@ -92,7 +126,7 @@ class Plan(BaseModel):
     plan: str = Field(
         description="A brief description of the plan to solve the problem."
     )
-    tool: str = Field(description="The tool used to solve the problem.")
+    tool_name: str = Field(description="The tool used to solve the problem.")
     tool_input: str = Field(description="The input query to be sent to the tool.")
 
 
@@ -116,19 +150,19 @@ class ReWOO(TypedDict):
     # -------------------------
     planner_chain: RunnableSequence
     # -------------------------
-    plans_executed: Dict[int, PlanExecuted]
+    plans: Dict[str, PlanExecuted]
     # -------------------------
     question_answer: str
     # -------------------------
     current_step: int
-    step_history: List[int]
+    total_steps: int
+    plan_history: List[str]
 
 
 planner_prompt = ChatPromptTemplate.from_messages(
     [
-        {
-            "role": "system",
-            "content": """
+        SystemMessagePromptTemplate.from_template(
+            template="""
                 For the following task, break it down into a sequence of minimal, logically necessary plan step by step, 
                 each of which solves exactly one small sub-problem.
                 For each sub-problem, indicate which external tool together with tool input to retrieve evidence. 
@@ -144,61 +178,66 @@ planner_prompt = ChatPromptTemplate.from_messages(
                 with Wolfram Alpha. evidence 1 = WolframAlpha[Solve x + (2x - 10) + ((2x - 10) - 8) = 157]
                 Plan: Find out the number of hours Thomas worked. evidence 2 = LLM[What is x, given evidence 1]
                 Plan: Calculate the number of hours Rebecca worked. evidence = Calculator[(2 * evidence 2 - 10) - 8]
-            """,
-        },
-        {
-            "role": "human",
-            "content": """
+            """
+        ),
+        HumanMessagePromptTemplate.from_template(
+            template="""
                 Begin! 
                 Describe your plans with rich details. Each Plan should be followed by only one evidence.
 
                 Task: {task}
-            """,
-        },
+            """
+        ),
     ]
 )
-tool_desc = "\n                ".join(
-    [
-        """{tool_name}:  {tool_desc}""".format(
-            tool_name=tool.name, tool_desc=tool.description
-        )
-        for tool in tools
-    ]
-)
-# task = "hello what can you do for me?"
 
 
 # %%
 def get_plan(state: ReWOO):
     planned_results = state["planner_chain"].invoke(
-        {"task": state["question"], "tool_desc": state["tools_desc"]}
+        {
+            "task": state["question"],
+            "tool_desc": state["tools_desc"],
+            "plan_history": state["plan_history"],
+        }
     )
     return {
-        "plans_executed": {
-            plan.order: {
+        "total_steps": len(planned_results.plans),
+        "plans": {
+            str(plan.order): {
                 "plan": plan.plan,
-                "tool_name": plan.tool,
+                "tool_name": plan.tool_name,
                 "tool_input": plan.tool_input,
             }
             for plan in planned_results.plans
-        }
+        },
     }
 
 
 def tool_execution(state: ReWOO):
-    return {
-        "plans_executed": {
-            order: {
-                **plan_detail,
-                **{
-                    "tool_executed_result": state["tools"][
-                        plan_detail["tool_name"]
-                    ].invoke({"query": plan_detail["tool_input"]})
-                },
-            }
-            for order, plan_detail in state["plans_executed"].items()
+    step = str(state["current_step"])
+    plan_detail = state["plans"][step]
+    tool_execute_result = state["tools"][plan_detail["tool_name"]].invoke(
+        {
+            "query": redefine_tool_input.invoke(
+                {
+                    "query": plan_detail["tool_input"],
+                    "history_results": state["plan_history"],
+                }
+            ).content
         }
-    }
+    )
+    state["plans"][step].update({"tool_executed_result": tool_execute_result.content})
+    state["plan_history"].append(tool_execute_result.content)
+
+    return {"current_step": state["current_step"] + 1}
+
+
+def continue_tool_execute(state: ReWOO):
+    if state["current_step"] > state["total_steps"]:
+        return END
+    else:
+        return "tool execute"
 
 
 # %%
@@ -206,11 +245,9 @@ def tool_execution(state: ReWOO):
 graph = StateGraph(ReWOO)
 graph.add_node("plan", get_plan)
 graph.add_node("tool execute", tool_execution)
-# graph.add_node("solve", solve)
-# graph.add_edge("solve", END)
-# graph.add_conditional_edges("tool", _route)
-graph.add_edge(START, "plan")
-graph.add_edge("plan", "tool execute")
+graph.add_edge(start_key=START, end_key="plan")
+graph.add_edge(start_key="plan", end_key="tool execute")
+graph.add_conditional_edges(source="tool execute", path=continue_tool_execute)
 
 app = graph.compile()
 result = app.invoke(
@@ -226,6 +263,8 @@ result = app.invoke(
             ]
         ),
         "planner_chain": planner_prompt | model.with_structured_output(PlanResponse),
+        "current_step": 1,
+        "plan_history": [],
     }
 )
 
