dd9bc4c94c2866b7f1f0b4820169743fc8c4e2a1
arthur <wr80340@smail.nchu.edu.tw>
2025-04-13 12:19:01 +0800

    #add graph with plan and execute

--- advance_agent_test.py
+++ advance_agent_test.py
@@ -3,7 +3,7 @@ import json
 
 from IPython.display import Image, display
 
-from agent_framework.core.agent.pg_to_qdrant_agent import table_summary_upsert_agent
+from agent_framework.agent.pg_to_qdrant_agent import table_summary_upsert_agent
 
 with open("secrets.json") as f:
     secrets = json.loads(f.read())
@@ -29,4 +29,14 @@ a = agent.invoke(
 
 # %%
 a
+# %%
+import numpy as np
+import pandas as pd
+from ydata_profiling import ProfileReport
+
+df = pd.read_csv("att_rawdata_202503310947.csv")
+# %%
+df.select_dtypes(include="object").aggregate(
+    func=lambda x: list(np.unique(x)), axis=0
+).to_dict()
 # %%
--- langgraph_tutorials/rewoo.py
+++ langgraph_tutorials/rewoo.py
@@ -2,13 +2,16 @@
 import json
 import os
 import re
-from typing import List
+from typing import Dict, List
 
 from IPython.display import Image, display
 from langchain.tools import tool
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables.base import RunnableSequence
+from langchain_core.tools.structured import StructuredTool
 from langchain_ollama import ChatOllama
+from langgraph.graph import END, START, StateGraph
 from pydantic import BaseModel, Field
 from typing_extensions import TypedDict
 
@@ -22,28 +25,64 @@ with open("../config.json") as f:
 os.environ["LANGSMITH_API_KEY"] = secrets.get("langsmith").get("api_key")
 os.environ["TAVILY_API_KEY"] = secrets.get("tavily").get("api_key")
 
-online_search = TavilySearchResults(max_results=5)
+
 model = ChatOllama(model=config.get("llm_model").get("model_name"), temperature=0)
+travily_search = TavilySearchResults(max_results=3)
 
 
 @tool
-def summarize_results(query: str):
-    "A LLM model for summarizing the results"
-    return model.invoke({"query": query})
+def chat_bot(query: str):
+    """
+    You are a smart and friendly AI assistant. You handle general conversations, tasks, and instructions normally and directly.
+    However, when a user asks a factual or knowledge-based question — especially about current events, news, specific information, or anything that may require up-to-date facts — you must NOT answer it yourself.
+    """
+
+
+@tool
+def travily_result_summarize(query: str):
+    """
+    A search engine optimized for comprehensive, accurate, and trusted results.
+    Useful for when you need to answer questions about current events.
+    Input should be a search query.
+    And will summarizes and answers base on user questions.
+    """
+    travily_search = TavilySearchResults(max_results=5)
+    search_result_prompt = ChatPromptTemplate.from_messages(
+        [
+            {
+                "role": "human",
+                "content": """
+                    You are a smart assistant that answers questions based on search engine results.
+                    Given a user question and a list of search results retrieved from Travily, 
+                    extract the most relevant information to directly answer the question.
+                    Keep your answer concise and factual. Include important numbers or descriptions if available. 
+                    Do not add extra explanations or assumptions.
+                    """,
+            },
+            {
+                "role": "human",
+                "content": """
+                    User Question:
+                    {query}
+
+                    Travily Search Results (sorted by relevance):
+                    {search_results}
+
+                    Based on the above information, provide a direct answer to the question. 
+                    If no relevant answer can be found, reply with: "The answer is not available based on the current search results."
+                """,
+            },
+        ]
+    )
+    chain = search_result_prompt | model
+
+    return chain.invoke(
+        {"query": query, "search_results": travily_search.invoke({"query": query})}
+    )
 
 
 # %%
-tools = [online_search, summarize_results]
-# %%
-
-
-class ReWOO(TypedDict):
-    task: str
-    plan_string: str
-    steps: List
-    results: dict
-    result: str
-
+tools = [chat_bot, travily_result_summarize]
 
 # %%
 
@@ -57,17 +96,42 @@ class Plan(BaseModel):
     tool_input: str = Field(description="The input query to be sent to the tool.")
 
 
-class Response(BaseModel):
+class PlanResponse(BaseModel):
     plans: List[Plan] = Field(description="Plan to solve the question.")
 
 
-prompt = ChatPromptTemplate.from_messages(
+class PlanExecuted(TypedDict):
+    plan: str
+    tool_name: str
+    tool_input: str
+    tool_executed_result: str
+
+
+class ReWOO(TypedDict):
+    # -------------------------
+    question: str
+    # -------------------------
+    tools: Dict[str, StructuredTool]
+    tools_desc: str
+    # -------------------------
+    planner_chain: RunnableSequence
+    # -------------------------
+    plans_executed: Dict[int, PlanExecuted]
+    # -------------------------
+    question_answer: str
+    # -------------------------
+    current_step: int
+    step_history: List[int]
+
+
+planner_prompt = ChatPromptTemplate.from_messages(
     [
         {
             "role": "system",
             "content": """
-                For the following task, make plans that can solve the problem step by step. For each plan, indicate \
-                which external tool together with tool input to retrieve evidence. 
+                For the following task, break it down into a sequence of minimal, logically necessary plan step by step, 
+                each of which solves exactly one small sub-problem.
+                For each sub-problem, indicate which external tool together with tool input to retrieve evidence. 
 
                 Tools can be one of the following:
                 {tool_desc}
@@ -93,7 +157,7 @@ prompt = ChatPromptTemplate.from_messages(
         },
     ]
 )
-tool_desc = "\n".join(
+tool_desc = "\n                ".join(
     [
         """{tool_name}:  {tool_desc}""".format(
             tool_name=tool.name, tool_desc=tool.description
@@ -101,15 +165,70 @@ tool_desc = "\n".join(
         for tool in tools
     ]
 )
-task = "what is the exact hometown of the 2024 mens australian open winner"
-model_with_structured_output = model.with_structured_output(Response)
-chain = prompt | model_with_structured_output
-result = chain.invoke({"task": task, "tool_desc": tool_desc})
+# task = "hello what can you do for me?"
+
 
 # %%
-# result.plans[0].plan
-# result.plans[0].tool
-result.plans[0].tool_input
+def get_plan(state: ReWOO):
+    planned_results = state["planner_chain"].invoke(
+        {"task": state["question"], "tool_desc": state["tools_desc"]}
+    )
+    return {
+        "plans_executed": {
+            plan.order: {
+                "plan": plan.plan,
+                "tool_name": plan.tool,
+                "tool_input": plan.tool_input,
+            }
+            for plan in planned_results.plans
+        }
+    }
 
 
+def tool_execution(state: ReWOO):
+    return {
+        "plans_executed": {
+            order: {
+                **plan_detail,
+                **{
+                    "tool_executed_result": state["tools"][
+                        plan_detail["tool_name"]
+                    ].invoke({"query": plan_detail["tool_input"]})
+                },
+            }
+            for order, plan_detail in state["plans_executed"].items()
+        }
+    }
+
+
+# %%
+
+graph = StateGraph(ReWOO)
+graph.add_node("plan", get_plan)
+graph.add_node("tool execute", tool_execution)
+# graph.add_node("solve", solve)
+# graph.add_edge("solve", END)
+# graph.add_conditional_edges("tool", _route)
+graph.add_edge(START, "plan")
+graph.add_edge("plan", "tool execute")
+
+app = graph.compile()
+result = app.invoke(
+    {
+        "question": "what is the exact hometown of the 2024 mens australian open winner",
+        "tools": {tool.name: tool for tool in tools},
+        "tools_desc": "\n                ".join(
+            [
+                """{tool_name}:  {tool_desc}""".format(
+                    tool_name=tool.name, tool_desc=tool.description
+                )
+                for tool in tools
+            ]
+        ),
+        "planner_chain": planner_prompt | model.with_structured_output(PlanResponse),
+    }
+)
+
+# %%
+result
 # %%
